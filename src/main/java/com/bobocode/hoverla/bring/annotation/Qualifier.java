cc271a014e7a35de4f03815bdfb53d7647df0bb0
r.kholodnytskyi <rstkhldntsk97@outlook.com>
2021-11-10 16:48:57 +0100

    Added retention policy to current annotations

--- src/main/java/com/bobocode/hoverla/bring/annotation/Qualifier.java
+++ src/main/java/com/bobocode/hoverla/bring/annotation/Qualifier.java
@@ -1,9 +1,13 @@
 package com.bobocode.hoverla.bring.annotation;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.TYPE})
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.TYPE, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Qualifier {
+
     String value() default "";
 }
