bc4131e621202896d8345bf3fd82e93055b9314c
Andriy Androsyuk <andriiandrosiukk@gmail.com>
2021-11-24 07:50:10 +0100

    delete bring project

--- src/main/java/com/bobocode/hoverla/bring/annotation/Autowired.java
+++
@@ -1,8 +0,0 @@
-package com.bobocode.hoverla.bring.annotation;
-
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Target;
-
-@Target({ElementType.METHOD, ElementType.CONSTRUCTOR, ElementType.FIELD})
-public @interface Autowired {
-}
--- src/main/java/com/bobocode/hoverla/bring/annotation/Qualifier.java
+++
@@ -1,9 +0,0 @@
-package com.bobocode.hoverla.bring.annotation;
-
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Target;
-
-@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.TYPE})
-public @interface Qualifier {
-    String value() default "";
-}
